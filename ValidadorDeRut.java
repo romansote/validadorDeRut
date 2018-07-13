1f83cc7120adb3ddb6a5defda8f7544f9fdfa0fd
romansote <roman.mardones@gmail.com>
2018-07-13 09:52:11 -0400

    Update ValidadorDeRut.java

--- ValidadorDeRut.java
+++ ValidadorDeRut.java
@@ -1,4 +1,5 @@
 
+
 package verificador.de.rut;
 
 import java.util.Scanner;
@@ -8,76 +9,86 @@ import java.util.Scanner;
  * @author roman
  * @version 1.0
  */
-public class ValidadorDeRut {
-
+public class Persona {
     int suma;
     int resto;
     int resultado;
     int multiplicacion;
+    int[] numfijos = {3, 2, 7, 6, 5, 4, 3, 2};
     String digitoVerificador;
     String capVerificador;
     String res;
-    int[] numfijos = {3, 2, 7, 6, 5, 4, 3, 2};
+    String rut;
 
-    public ValidadorDeRut() {
+    public Persona() {
         this.suma = 0;
     }
 
-    public void verificarRut(){
+    public void verificarRut()
+    {
         Scanner sc1 = new Scanner(System.in);
         
         System.out.println("#####  Verificador de rut #######");
         System.out.println("");
         System.out.println("Ingrese su rut [Formato: 12345678-9]");
-        String rut = sc1.nextLine();
+        rut = sc1.nextLine();
         
-        if(rut.length() != 10){
+        if(rut.length() != 10)
+        {
             System.out.println("Formato de su rut invalido!");
-        }else{
+        }
+        else
+        {
             for(int i =0; i < numfijos.length; i++)
             {
-               multiplicacion = Integer.parseInt(rut.substring(i,i+1)) * numfijos[i];
-               
-               suma += multiplicacion;
+                multiplicacion = Integer.parseInt(rut.substring(i,i+1)) * numfijos[i];
+                 
+                suma += multiplicacion;
             }
             
             resto = suma % 11;
             
             resultado = (11-resto);
             
-            switch (resultado) {
-                case 11:
-                    digitoVerificador ="0";
-                    if(digitoVerificador != capVerificador){
-                        System.out.println("El digito verificador ingresado es invalido, su digito verificador es = "+digitoVerificador);
-                    }else{
-                        System.out.println("Digito verificador valido");
-                    }   
-                    break;
-                case 10:
-                    digitoVerificador = "k";
-                    if(digitoVerificador != capVerificador){
-                        System.out.println("El digito verificador ingresado es invalido, su digito verificador es ="+digitoVerificador);
-                    }else{
-                        System.out.println("Digito verificador valido");
-                    }   
-                    break;
-                default:
-                    capVerificador = rut.substring(10);
-                    
-                    res= Integer.toString(resultado);
-                    
-                    System.out.println(res);
-                    
-                    if(res != capVerificador){
-                        
-                        System.out.println("El digito verificador ingresado es invalido, su digito verificador es ="+res);
-                    }else{
-                        System.out.println("Digito verificador valido");
-                    }     
-                    break;
-            }
+            if(resultado == 11)
+            {
+                digitoVerificador ="0";
+                
+                if(digitoVerificador.equals(capVerificador))
+                {
+                    System.out.println("El digito verificador ingresado es invalido, su digito verificador es ="+digitoVerificador);
+                }else
+                {
+                    System.out.println("Digito verificador valido");
+                }
+            }else if(resultado == 10)
+            {
+                digitoVerificador = "k";
+                
+                if(!digitoVerificador.equals(capVerificador))
+                {
+                    System.out.println("El digito verificador ingresado es invalido, su digito verificador es ="+digitoVerificador);
+                }else
+                {
+                    System.out.println("Digito verificador valido");
+                }
+                
+            }else
+            {
+              capVerificador = rut.substring(9);
+              
+              res= Integer.toString(resultado);
+              
+              if(res.equals(capVerificador))
+              {
+                  System.out.println("Digito verificador valido");
+              }else
+              {
 
+                  System.out.println("El digito verificador ingresado es invalido, su digito verificador es ="+res);
+              }
+                
+            }
         }
     }
 }
